bc18f0c2d1ff64642f5a3f45d632f17f689b32a8
Adam Walker <adamw207@yahoo.com>
2019-03-09 18:22:52 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,6 @@
-speak_to_grandma = "HUH?! SPEAK UP, SONNY!" 
-  if speak_to_grandma == "hungry" puts "Refilling food bowl." 
-    elsif speak_to_grandma == "thirsty" puts "Refilling water bowl" 
-    elsif speak_to_grandma == "cuddly" puts "HUH?! SPEAK UP, SONNY!" 
+speak_to_grandma = "I LOVE YOU GRANDMA!" 
+  if speak_to_grandma == .upcase puts "NO, NOT SINCE 1938!" 
+  elsif speak_to_grandma == "I LOVE YOU GRANDMA!" puts "I LOVE YOU TOO PUMPKIN!" 
   
   else puts "HUH?! SPEAK UP, SONNY!" end
 
