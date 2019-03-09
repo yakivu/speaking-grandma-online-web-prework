da4560b0a20b02a3a95d78a3b0453c34ba2a6786
Adam Walker <adamw207@yahoo.com>
2019-03-09 18:04:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,8 @@
-# Write a speak_to_grandma method.
+speak_to_grandma = "cuddly" 
+  if dog == "hungry" puts "Refilling food bowl." 
+    elsif dog == "thirsty" puts "Refilling water bowl" 
+    elsif dog == "playful" puts "Playing tug-of-war." 
+    elsif dog == "cuddly" puts "Snuggling." 
+  
+  else puts "Reading newspaper." end
 
-# Whatever you say to grandma, she should respond with
-# HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
-
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
-
-# NO, NOT SINCE 1938!
-
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
