e2b352e46f17d2cd2245b1bb943dca88467f2988
Adam Walker <adamw207@yahoo.com>
2019-03-09 18:24:41 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 speak_to_grandma = "I LOVE YOU GRANDMA!" 
-  if speak_to_grandma == .upcase puts "NO, NOT SINCE 1938!" 
+  if speak_to_grandma == .upcase! puts "NO, NOT SINCE 1938!" 
   elsif speak_to_grandma == "I LOVE YOU GRANDMA!" puts "I LOVE YOU TOO PUMPKIN!" 
   
   else puts "HUH?! SPEAK UP, SONNY!" end
